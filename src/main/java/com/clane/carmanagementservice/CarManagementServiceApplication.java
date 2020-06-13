aba84f4a467a26c6ae4e3c26f3f92e771d934c5f
yemifirefox <yemifirefox@yahoo.com>
2020-06-13 01:38:19 +0100

    Read me file updated

--- src/main/java/com/clane/carmanagementservice/CarManagementServiceApplication.java
+++ src/main/java/com/clane/carmanagementservice/CarManagementServiceApplication.java
@@ -2,31 +2,14 @@ package com.clane.carmanagementservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaAuditing
-
 public class CarManagementServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarManagementServiceApplication.class, args);
-	}
-
-
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any())
-				.build();
 	}
 
 }
